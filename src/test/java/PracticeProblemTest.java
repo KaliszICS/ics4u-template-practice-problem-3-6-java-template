0180c07081136fc12aa95ec86d4abdca58efc267
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-04-07 19:14:25 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,49 +1,200 @@
+import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.io.*;
-
 public class PracticeProblemTest {
+@Test
+@DisplayName("")
+void SearchMazeMovesTest4() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("searchMazeMoves", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"S", "*", "F"},
+  };
+    assertEquals(-1, (int)method.invoke(null, new Object[]{maze}));
+}
 
-   @Test
-   public void testOutput()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
 
-     // action
-     PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void MathPathsTest4() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("noOfPaths", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"S", "*", "F"},
+  };
+    assertEquals(0, (int)method.invoke(null, new Object[]{maze}));
+}
 
-     // assertion
-     assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void MathPathsTest3() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("noOfPaths", cArg);
+  // Enter code here
+    String[][] maze = {
+    {"", "", "*", "F"},
+    {"", "", "*", ""},
+    {"", "", "*", ""},
+    {"S", "", "", ""},
+  };
+    assertEquals(1, (int)method.invoke(null, new Object[]{maze}));
+}
 
-   @Test
-   public void testInputandOutput()
-   {
-      String data = "Users Input";
-      System.setIn(new ByteArrayInputStream(data.getBytes()));
-      
-      PrintStream originalOut = System.out;
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos));
 
-      // action
-      PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void MazePathsTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("noOfPaths", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"", "", "", "", ""},
+    {"", "*", "F", "", ""},
+    {"", "", "", "", ""},
+    {"S", "", "", "", ""},
+  };
+    assertEquals(3, (int)method.invoke(null, new Object[]{maze}));
+}
 
-      // assertion
-      assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-      // undo the binding in System
-      System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void MazePathsTest1() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("noOfPaths", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"", "", "", "", ""},
+    {"", "", "", "", ""},
+    {"", "", "", "F", ""},
+    {"S", "*", "*", "", ""},
+  };
+    assertEquals(1, (int)method.invoke(null, new Object[]{maze}));
+}
 
-   @Test
-   public void testQ3()
-   {
-     
-   }
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void searchMazeMovesTest1() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("searchMazeMoves", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"", "", "", ""},
+    {"", "", "", ""},
+    {"", "", "", "F"},
+    {"S", "*", "*", "*"},
+  };
+    assertEquals(4, (int)method.invoke(null, new Object[]{maze}));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void SearchMazeMovesTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("searchMazeMoves", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"", "", "", "", ""},
+    {"", "", "", "", ""},
+    {"", "", "*", "F", ""},
+    {"S", "*", "", "", ""},
+  };
+    assertEquals(-1, (int)method.invoke(null, new Object[]{maze}));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void SearchMazeMovesTest3() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class<?>[] cArg = {String[][].class};
+        Method method = testClass.getDeclaredMethod("searchMazeMoves", cArg);
+  // Enter code here
+  String[][] maze = {
+    {"", "", "F", "", ""},
+    {"", "*", "", "", ""},
+    {"", "*", "", "", ""},
+    {"S", "*", "", "", ""},
+  };
+    assertEquals(5, (int)method.invoke(null, new Object[]{maze}));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
 }
